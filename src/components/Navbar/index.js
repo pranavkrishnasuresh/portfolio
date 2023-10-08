acd83a793197e1d5d17cf53c164848966d214925
Pranavkrishna Suresh <85195581+pranavkrishnasuresh@users.noreply.github.com>
2023-10-08 15:17:09 -0400

    GitHub link fix

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,6 +5,7 @@ import { Link as LinkR } from 'react-router-dom';
 import { IoCodeSlashSharp } from "react-icons/io5";
 import { FaBars } from 'react-icons/fa';
 import { useTheme } from 'styled-components';
+import { Bio } from '../../data/constants';
 
 const Nav = styled.div`
     background-color: ${({ theme }) => theme.card_light};
@@ -163,52 +164,52 @@ const MobileMenuLinks = styled(LinkR)`
 
 
 const Navbar = () => {
-    const [open, setOpen] = React.useState(false);
-    const theme = useTheme()
-    return (
-        <Nav>
-            <NavContainer>
-                <NavLogo to='/'>
-                    <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: '20;', cursor: 'pointer' }}>
-                        <IoCodeSlashSharp size="3rem" /> <Span>Portfolio</Span>
-                    </a>
-                </NavLogo>
-                <MobileIcon>
-                    <FaBars onClick={() => {
-                        setOpen(!open)
-                    }} />
-                </MobileIcon>
-                <NavItems>
-                    <NavLink href="#about">About</NavLink>
-                    <NavLink href='#education'>Education</NavLink>
-                    <NavLink href='#skills'>Skills</NavLink>
-                    <NavLink href='#experience'>Experience</NavLink>
-                    <NavLink href='#projects'>Projects</NavLink>
-                </NavItems>
-                <ButtonContainer>
-                    <GitHubButton>Github Profile</GitHubButton>
-                </ButtonContainer>
-            </NavContainer>
-            {open && <MobileMenu open={open}>
-                <MobileMenuLinks href="#about" onClick={() => {
-                    setOpen(!open)
-                }}>About</MobileMenuLinks>
-                <MobileMenuLinks href='#skills' onClick={() => {
-                    setOpen(!open)
-                }}>Skills</MobileMenuLinks>
-                <MobileMenuLinks href='#experience' onClick={() => {
-                    setOpen(!open)
-                }}>Experience</MobileMenuLinks>
-                <MobileMenuLinks href='#projects' onClick={() => {
-                    setOpen(!open)
-                }}>Projects</MobileMenuLinks>
-                <MobileMenuLinks href='#education' onClick={() => {
-                    setOpen(!open)
-                }}>Education</MobileMenuLinks>
-                <GitHubButton style={{ padding: '10px 16px', background: `${theme.primary}`, color: 'white', width: 'max-content' }} href="https://github.com/pranavkrishnasuresh" target="_blank">Github Profile</GitHubButton>
-            </MobileMenu>}
-        </Nav>
-    );
+  const [open, setOpen] = React.useState(false);
+  const theme = useTheme()
+  return (
+    <Nav>
+      <NavContainer>
+        <NavLogo to='/'>
+          <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: '20;', cursor: 'pointer' }}>
+            <IoCodeSlashSharp size="3rem" /> <Span>Portfolio</Span>
+          </a>
+        </NavLogo>
+        <MobileIcon>
+          <FaBars onClick={() => {
+            setOpen(!open)
+          }} />
+        </MobileIcon>
+        <NavItems>
+          <NavLink href="#about">About</NavLink>
+          <NavLink href='#education'>Education</NavLink>
+          <NavLink href='#skills'>Skills</NavLink>
+          <NavLink href='#experience'>Experience</NavLink>
+          <NavLink href='#projects'>Projects</NavLink>
+        </NavItems>
+        <ButtonContainer>
+          <GitHubButton href={Bio.github} target="_blank">Github Profile</GitHubButton>
+        </ButtonContainer>
+      </NavContainer>
+      {open && <MobileMenu open={open}>
+        <MobileMenuLinks href="#about" onClick={() => {
+          setOpen(!open)
+        }}>About</MobileMenuLinks>
+        <MobileMenuLinks href='#skills' onClick={() => {
+          setOpen(!open)
+        }}>Skills</MobileMenuLinks>
+        <MobileMenuLinks href='#experience' onClick={() => {
+          setOpen(!open)
+        }}>Experience</MobileMenuLinks>
+        <MobileMenuLinks href='#projects' onClick={() => {
+          setOpen(!open)
+        }}>Projects</MobileMenuLinks>
+        <MobileMenuLinks href='#education' onClick={() => {
+          setOpen(!open)
+        }}>Education</MobileMenuLinks>
+        <GitHubButton style={{ padding: '10px 16px', background: `${theme.primary}`, color: 'white', width: 'max-content' }} href={Bio.github} target="_blank">Github Profile</GitHubButton>
+      </MobileMenu>}
+    </Nav>
+  );
 };
 
 export default Navbar;
